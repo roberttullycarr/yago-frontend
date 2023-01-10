943eb3b060e64a8246e3738a18873d5c10ff5024
Robert Carr <robert@pitchdrive.com>
2023-01-10 10:40:03 +0100

    added redirect from base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/", // automatically handles all locales
+        destination: "/lead/create_quote", // automatically passes the locale on
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
